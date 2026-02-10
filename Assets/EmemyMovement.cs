4c482f5e29feeac8e1768df765f3e285bc081562
IsaacM-git <6778vptd7c@privaterelay.appleid.com>
2026-02-10 13:41:04 -0500

    Updated Enemy Movement Script
    Updated enemy movement script so the enemy bounces between walls.
    2/10/2026 1:39 PM

--- Assets/EmemyMovement.cs
+++ Assets/EmemyMovement.cs
@@ -6,24 +6,31 @@ public class EmemyMovement : MonoBehaviour
 
     private Vector2 movementDirection;
 
-    private Rigidbody2D rb; 
+    //private Rigidbody2D rb; 
     private Rigidbody2D parentRb;
 
+    public Transform childTransform;
+    public Vector3 positionLeft = new Vector3(-0.5f, 0, 0);
+    public Vector3 positionRight = new Vector3(0.5f, 0, 0);
 
-    private int xDir = 1; // -1 = left, 1 = right
+
+    private int xDir = -1; // -1 = left, 1 = right
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-       rb = GetComponent<Rigidbody2D>();
-       parentRb = transform.parent.GetComponent<Rigidbody2D>();
+       //rb = transform.GetComponent<Rigidbody2D>();
+        parentRb = transform.parent.GetComponent<Rigidbody2D>();
+        
        
     }
 
     // Update is called once per frame
     void Update()
     {
-        rb.linearVelocity = new Vector2(xDir * movementSpeed, rb.linearVelocity.y);
+        //rb.linearVelocity = new Vector2(xDir * movementSpeed, rb.linearVelocity.y);
         parentRb.linearVelocity = new Vector2(xDir * movementSpeed, parentRb.linearVelocity.y);
+    
+        //parentRb.linearVelocity = new Vector2(xDir * movementSpeed, parentRb.linearVelocity.y);
 
         //movementDirection = new Vector2(movementSpeed*xDir, 0f); 
     }
@@ -32,12 +39,20 @@ public class EmemyMovement : MonoBehaviour
         // Check if the object that entered has a specific tag
         if (other.gameObject.CompareTag("Floor"))// (enemy.CompareTag("floor"))
         {
+            if (childTransform.localPosition == positionLeft)
+            {
+                childTransform.localPosition = positionRight;
+            }
+            else
+            {
+                childTransform.localPosition = positionLeft;
+            }
             Debug.Log("Player entered the trigger area!");
             xDir *= -1;
-            rb.linearVelocity = new Vector2(xDir * movementSpeed, 0);
-            parentRb.linearVelocity = new Vector2(xDir * movementSpeed, 0);
+            //rb.linearVelocity = new Vector2(xDir * movementSpeed, 0);
+            // parentRb.linearVelocity = new Vector2(xDir * movementSpeed, 0);
             //transform.Translate(Vector3.up * 100);
-            transform.parent.Translate(Vector3.up * 5);
+            //transform.parent.Translate(Vector3.up * 5);
             
             //transform.parent
 
