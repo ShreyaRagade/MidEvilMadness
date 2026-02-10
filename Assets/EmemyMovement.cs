b245c0767e86a88c0ceb7e1d2d81c8855f50c080
IsaacM-git <6778vptd7c@privaterelay.appleid.com>
2026-02-09 21:37:54 -0500

    EnemyMovement
    Start of the EnemyMovement script
    2/6/2026 10:52

---
+++ Assets/EmemyMovement.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+
+public class EmemyMovement : MonoBehaviour
+{
+    private float movementSpeed = 5f;
+
+    private Vector2 movementDirection;
+
+    private Rigidbody2D rb; 
+    private Rigidbody2D parentRb;
+
+
+    private int xDir = 1; // -1 = left, 1 = right
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+       rb = GetComponent<Rigidbody2D>();
+       parentRb = transform.parent.GetComponent<Rigidbody2D>();
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.linearVelocity = new Vector2(xDir * movementSpeed, rb.linearVelocity.y);
+        parentRb.linearVelocity = new Vector2(xDir * movementSpeed, parentRb.linearVelocity.y);
+
+        //movementDirection = new Vector2(movementSpeed*xDir, 0f); 
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        // Check if the object that entered has a specific tag
+        if (other.gameObject.CompareTag("Floor"))// (enemy.CompareTag("floor"))
+        {
+            Debug.Log("Player entered the trigger area!");
+            xDir *= -1;
+            rb.linearVelocity = new Vector2(xDir * movementSpeed, 0);
+            parentRb.linearVelocity = new Vector2(xDir * movementSpeed, 0);
+            //transform.Translate(Vector3.up * 100);
+            transform.parent.Translate(Vector3.up * 5);
+            
+            //transform.parent
+
+            // Perform actions like playing sound, loading scene, etc.
+            //Vector3 parentPosition = transform.parent.position
+
+        }
+    }
+
+    /*
+    ontriggerenter
+
+
+
+    */
+}
