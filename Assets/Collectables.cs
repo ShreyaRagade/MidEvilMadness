f861b04f527e0bb0b5d48ac2b7d45dfc0886b909
ohelali01 <o.h082207@gmail.com>
2026-02-09 20:57:58 -0500

    Assets + Collectables Script
    Tileset Assets and Collectables Script for money collection and storing across multiple scripts

---
+++ Assets/Collectables.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class Collectables : MonoBehaviour
+{
+    public static int coins = 0;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("This is a collectable!");
+        coins = coins + 1;
+        Debug.Log("Coins: " + coins);
+        Destroy(gameObject);
+    }
+}
