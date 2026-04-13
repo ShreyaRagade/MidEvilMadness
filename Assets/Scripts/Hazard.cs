39455a4b57fd7d3ea2a8465f5df62d98e1509655
Liv0412 <olivia0412martin@gmail.com>
2026-04-13 13:48:24 -0400

    4/13/2026
    Lets Hazard.cs check if a player touches it and damages them if it does.

--- Assets/Scripts/Hazard.cs
+++ Assets/Scripts/Hazard.cs
@@ -3,14 +3,25 @@ using UnityEngine;
 public class Hazard : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    public int hazardDamage = 10;
+
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Health.HealthInstance.healthAmount -= hazardDamage;
+        }
     }
 }
