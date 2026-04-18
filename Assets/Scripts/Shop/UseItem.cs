27cf34cf9de4e13ae9b6357fa79b51ae5c25249b
ShreyaRagade <sragade3@gmail.com>
2026-04-18 19:12:28 -0400

    4/18 Added Base Scripts for Shop System
    Added the files:
    CurrencyController.cs
    ShopController.cs
    ShopNPC.cs
    ShopSlot.cs
    UseItem.cs
    Which are the basis for the Shop System

---
+++ Assets/Scripts/Shop/UseItem.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using DoubleTechniStyle;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+[CreateAssetMenu(menuName = "Inventory/Equipment")]
+public class UseItem : ScriptableObject
+{
+    public string itemName;
+    public int generalItemID;
+    public string uniqueItemID;
+
+    public GameObject itemPrefab; //prolly get rid of this
+
+    public int HP;
+    
+
+    [TextArea(1, 5)]
+    public string itemDescription; //make sure you include the stats with this and the name, like Kombucha (DELL +100)
+
+
+    //Shop
+    public int buyPrice = 10;
+
+    [Range(0, 1)]
+    public float sellPriceMultiplier = 0.5f;
+
+    
+    private void OnEnable()
+    {
+
+        if (string.IsNullOrEmpty(uniqueItemID))
+        {
+            uniqueItemID = itemName + Guid.NewGuid().ToString();
+
+        }
+
+    }
+
+    public virtual void Use()
+    {
+        //use the item
+
+        Debug.Log("using " + name);
+
+        Health.HealthInstance.healthAmount = HP;
+       
+
+    }
+
+    
+
+
+}
+
+
