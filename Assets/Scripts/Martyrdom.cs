c9fdb1d19c26ce344b6a17a8ab599495481d9d90
IsaacM-git <6778vptd7c@privaterelay.appleid.com>
2026-04-30 16:01:07 -0400

    Finished New Enemy Attack 4/30/2026 @ 3:59PM
    Edited Hazard.cs to automatically grab the player and respawn point so the Drier's attack works as intended.

--- Assets/Scripts/Martyrdom.cs
+++ Assets/Scripts/Martyrdom.cs
@@ -10,40 +10,26 @@ public class Martyrdom : MonoBehaviour
 
     public GameObject targetObject;
 
-
-
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         parentRb = GetComponent<Rigidbody2D>();   
         parentRb.linearVelocity = new Vector2(0, upVelocity);
-        //Debug.Log("MADE MA");
         justMade = true;
-
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
     void OnCollisionEnter2D(Collision2D collision){
-        //Debug.Log("Collide M | " + collision.gameObject.CompareTag("Floor") + " | " + parentRb.linearVelocity.y + " | " + justMade);
         if (collision.gameObject.CompareTag("Player") || collision.gameObject.CompareTag("DrierEnemy") || collision.gameObject.CompareTag("Enemy")) {
             Physics2D.IgnoreCollision(collision.collider, GetComponent<Collider2D>());
         }
         if (collision.gameObject.CompareTag("Floor") && parentRb.linearVelocity.y <= 0f)
         {
-            //Debug.Log("boomish!");
+            Debug.Log("boomish!");
             GameObject clone = Instantiate(targetObject, transform.position, transform.rotation);
             Destroy(gameObject);
 
         }
         justMade = false;
-        //Debug.Log("justMade is now false");
-        //KABOOM
-        //Debug.Log("BOOM?");
-        //Destroy(parentRb);
     }
 
 }
