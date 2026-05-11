86e5702e6e765c91fdc6a485c9ed226b69cac273
ShreyaRagade <sragade3@gmail.com>
2026-05-10 21:42:17 -0400

    5/10 Added ScreenFader.cs, MainMenuNav.cs, PauseController.cs
    Added the following scripts:
    ScreenFader.cs
    MainMenuNav.cs
    PauseController.cs

---
+++ Assets/Scripts/Pause/ScreenFader.cs
@@ -0,0 +1,48 @@
+using System.Threading.Tasks;
+using UnityEngine;
+
+
+    public class ScreenFader : MonoBehaviour
+    {
+        public static ScreenFader Instance;
+        public CanvasGroup canvasGroup;
+        [SerializeField] float fadeDuration = 0.5f;
+
+
+        private void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+
+        }
+
+        async Task Fade(float targetTransparency)
+        {
+            float start = canvasGroup.alpha, t = 0;
+            while (t < fadeDuration)
+            {
+                t += Time.unscaledDeltaTime;
+                canvasGroup.alpha = Mathf.Lerp(start, targetTransparency, t / fadeDuration);
+                await Task.Yield();
+            }
+
+            canvasGroup.alpha = targetTransparency;
+        }
+
+        public async Task FadeOut()
+        {
+            await Fade(1); // fade to black
+        }
+
+        public async Task FadeIn()
+        {
+            await Fade(0); //fade to transparent
+        }
+    }
+
